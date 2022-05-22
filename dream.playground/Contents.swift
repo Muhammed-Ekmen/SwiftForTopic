801b0ebafca524bd421246275fe1110d21ee72b0
Muhammed-Ekmen <muhammedsemihekmen@hotmail.com>
2022-05-22 23:45:07 +0300

    push

--- dream.playground/Contents.swift
+++ dream.playground/Contents.swift
@@ -1,7 +1,36 @@
 import UIKit
 
-func total(numberOne:Int,numberTwo:Int)->Int{
-    return numberOne+numberTwo;
+
+
+class Users{
+    static var message:String="Is EveryThing Okey?"
+    var name:String?
+    var age:Int?
+    
+    init(name:String,age:Int) {
+        self.name=name
+        self.age=age
+    }
+    
+    func intro(){
+        print("Name:\(name!) Age:\(age!)")
+    }
+    
+    func total(numberOne:Int,NumberTwo:Int)->(total:Int,multiply:Int){
+        let total:Int=numberOne+NumberTwo
+        let multiply:Int=numberOne*NumberTwo
+        return (total,multiply)
+    }
+    
+    func totaler(numbers:Int...)->Int{
+        var starter:Int=0
+        for k in numbers{
+            starter+=k
+        }
+        return starter
+    }
+    
 }
 
-print(total(numberOne: 10, numberTwo: 20))
+var user_one=Users(name: "Semih", age: 22)
+
